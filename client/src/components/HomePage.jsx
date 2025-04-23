a6b8774312aeaf382b2b789724d54ef151515d06
georgetdn <gdobrescu@tdnsys.com>
2025-04-23 13:59:50 -0400

    Added About to the main page

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MenuBar from "./MenuBar";
 import ImageCarousel from "./ImageCarousel";
 import "../styles/HomePage.css";  // Import the override styles
+
 const HomePage = () => {
     return (
         <div className="home-container">
@@ -13,7 +14,45 @@ const HomePage = () => {
             
             {/* Main Content */}
             <div className="content">
-                <ImageCarousel />
+                <div className="carousel-wrapper">
+                    <ImageCarousel />
+                </div>
+            </div>  
+
+            {/* New Two Columns Section */}
+            <div className="two-columns">
+                <div className="column image-column">
+                    <img src="images/About.jpg" alt="Description" className="column-image" />
+                </div>
+                <div className="column text-column">
+                    <h2>About Y219.com</h2>
+                    <p><br/>We provide high-quality, affordable IT services for businesses and individuals. 
+From network setup and cybersecurity to software development, we deliver solutions tailored to your needs.
+<br/><br/>
+We specialize in cloud migration, data backup and recovery, and custom web applications.
+Our services are designed to scale as your business grows.
+Experience personalized support and innovative solutions with Y219.com.</p>
+                </div>
+                </div>
+                <div className="two-columns">
+                <div className="column text-column">
+        <h2>Our Service Area</h2>
+        <p><br/>
+        Y219.com proudly provides IT services to businesses and individuals across Northern Virginia.
+        We serve Alexandria, Fairfax, Arlington, and Prince William County with fast, reliable solutions.
+        <br/><br/>
+        Whether you need network setup, cybersecurity consulting, or custom software, we are ready to help.
+        <br/><br/>
+        Call us today at <strong>703-568-7739</strong> or email us at <strong>info@y219.com</strong> to learn more or schedule a consultation.
+        </p>
+    </div>
+                
+                
+                    <div className="column image-column">
+                        <img src="images/area.jpg" alt="Description" className="column-image" />
+                    </div>            
+
+
             </div>
 
             {/* Footer */}
@@ -25,4 +64,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
-
--- client/src/components/PricingPage.jsx
+++ client/src/components/PricingPage.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import PageLayout from "./PageLayout";
 
@@ -7,20 +8,20 @@ const PricingPage = () => {
             <h2>Pricing</h2>
             <div className="services-list">
                 <p>
-            <strong>Initial Consulation </strong>  Free <br/>   <br/>
-             <strong>Consulting Fee </strong>  $25/hour <br/>   <br/>
-             <strong>Linux and LibreOffice Installation</strong>  Free <br/>   <br/>
-             <strong>Telephone or Zoom Help Desk</strong>  $20/hour <br/>   <br/>
-             <strong>Custom Programming </strong>  Based on project length (about $125/day) <br/>   <br/>
-             <strong>Of the shelf software and hardware (to be purchased by the client): </strong> <br/>
+            <strong>Initial Consultation </strong>  -  Free <br/>    
+             <strong>Customer Support Fee </strong> -  $35/hour <br/>    
+             <strong>Linux and LibreOffice Installation</strong> -  Free <br/>    
+             <strong>Odoo Accounting Unlimited Use</strong> -  Free <br/>   
+             <strong>GNUcash Accounting Installation</strong> - Free<br/> 
+             <strong>Telephone or Zoom Help Desk</strong>  - $25/hour <br/>   
+             <strong>Custom Programming </strong> -  Based on project length (about $125/day) <br/>    
              <strong>Odoo</strong><br/>
              <a href="https://www.odoo.com/pricing/">Check prices here</a><br/>
              <strong>QuickBooks</strong><br/>
              <a href="https://quickbooks.intuit.com/pricing/">Check prices here</a><br/>
              <strong>Quicken</strong><br/>
              <a href="https://www.quicken.com/products/pricing-comparison/#pricing">Check prices here</a><br/>
-             <strong>GNUcash</strong><br/>Free<br/>
-             <strong>For Point of Sail hardware advice  <a href="../pos">click here</a></strong><b/><br/>
+             <strong>For  Point of Sale hardware advice  <a href="../pos">click here</a></strong><br/>
              <strong>For  Credit Card and payments advice <a href="../payments">click here</a></strong>
            </p>
             </div>
